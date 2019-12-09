0af42108f2877d505f14d5434619a3719aa8e312
ddelfaus <ddelfaus@gmail.com>
2019-12-09 17:29:52 -0500

    crud

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// const knex = require('./data/dbConfig.js');
+
+const mainRouter = require("./Routers/routerMain")
+
 
 const server = express();
 
 server.use(express.json());
+
+// server.get("/accounts", (req, res) => {
+
+//     knex
+//       .select("*")
+//       .from("accounts")
+//       .then(accounts => {
+//         res.status(200).json(accounts);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//         res.status(500).json({ errorMessage: "Error getting the accounts" });
+//       });
+//   });
+  
+
+server.use('/api/accounts', mainRouter)
 
 module.exports = server;
